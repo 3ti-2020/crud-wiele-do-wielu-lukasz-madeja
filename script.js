94d13c154aef9e4b45edd45f712959055ec93d01
maddking1337 <54845852+maddking1337@users.noreply.github.com>
2020-11-13 14:54:56 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('button').addEventListener("click", function() {
-	document.querySelector('.background-modal').style.display = "flex";
-});
-
-document.getElementById('close').addEventListener("click", function() {
-	document.querySelector('.background-modal').style.display = "none";
-});
